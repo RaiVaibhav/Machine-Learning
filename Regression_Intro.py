bc216f9d9fa738bcf1dc6c6b25b0b6c2a4be87e8
Vaibhav Rai <raivaibhav08@gmail.com>
2017-09-18 23:36:25 +0530

    Add files via upload
    Linear regression using Scikit learn with pickle file

---
+++ Regression_Intro.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import quandl ,math , datetime
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style 
+import pickle
+
+style.use('ggplot')
+
+quandl.ApiConfig.api_key = "W16koxS58dsK2EfZT3tp"
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100.0
+df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100.0
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume',]]
+##print(df.head())
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+x = np.array(df.drop(['label'],1))
+x = preprocessing.scale(x)
+x_lately  = x[-forecast_out:]
+x = x[:-forecast_out]
+
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2)
+
+##clf = LinearRegression(n_jobs=-1)
+##clf.fit(x_train, y_train)
+##confidence = clf.score(X_test, y_test)
+##print(confidence)
+##with open('linearregression.pickle','wb') as f:
+##	pickle.dump(clf, f)
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(x_test ,y_test)
+forecast_set = clf.predict(x_lately)
+
+##print(forecast_set, confidence, forecast_out)
+
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+
+print(df.head)
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
